d63afabae0856cd178a898103de36c85e0b69654
mostafa <mostafa.elsanadidy@gmail.com>
2019-10-07 10:57:37 -0700

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by mostafa elsanadidy on 10/7/19.
+//  Copyright © 2019 mostafa elsanadidy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
